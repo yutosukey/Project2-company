ad11d5526de9746a8c69100f7fb77fdf268456a9
Sukey <yutosukey@gmail.com>
2019-06-26 16:41:25 -0700

    payment js

--- yy-update/js/p2.js
+++ yy-update/js/p2.js
@@ -1,4 +1,6 @@
 
+console.log("dddd");
+
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -6,4 +8,19 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
+}
+
+function sccuess(){
+   
+  var x = document.getElementById("structure");
+  x.style.border = '10px solid red';
+  alert(x.className);
+  if (x.className === "successNone"){
+     x.className = "successDisplay"
+
+     alert(x.className);
+  }
+
+    //x.style.display = "block";
+ 
 }
