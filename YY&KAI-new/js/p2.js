cccbb7b762308ae719a6128d98410dd75c73fdfb
Sukey <yutosukey@gmail.com>
2019-06-26 21:10:53 -0700

    update details

--- YY&KAI-new/js/p2.js
+++ YY&KAI-new/js/p2.js
@@ -1,4 +1,6 @@
 
+console.log("dddd");
+
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -6,4 +8,26 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
+}
+
+function sccuess(){
+   
+  var x = document.getElementById("success");
+  x.style.display = "block";
+  x.style.position = "sticky";
+  x.style.top = "5em";
+
+    //x.style.display = "block";
+ 
+}
+
+function hide(){
+   
+  var x = document.getElementById("success");
+  x.style.display = "none";
+  x.style.position = "absolute";
+  x.style.top = "5em";
+
+    //x.style.display = "block";
+ 
 }
